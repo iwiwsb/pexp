e4bec3493c843946c474df9a1afe87f9a473e084
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2022-03-26 18:12:06 +0300

    Add section table struct

--- src/main.rs
+++ src/main.rs
@@ -220,7 +220,18 @@ impl TryFrom<u16> for Subsystem {
     }
 }
 
-struct SectionTable {}
+struct SectionTable {
+    name: String,
+    virtual_size: u32,
+    virtual_address: u32,
+    size_of_raw_data: u32,
+    pointer_to_raw_data: u32,
+    pointer_to_relocations: u32,
+    pointer_to_line_numbers: u32,
+    number_of_relocations: u32,
+    number_of_line_numbers: u32,
+    characteristics: u32,
+}
 
 struct DataDir {
     virtual_address: u32,
