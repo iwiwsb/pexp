b788c4ea7714730bfe6a6851ddc12cf79d715ae1
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2022-03-26 17:09:04 +0300

    Add data directories

--- src/main.rs
+++ src/main.rs
@@ -222,7 +222,29 @@ impl TryFrom<u16> for Subsystem {
 
 struct SectionTable {}
 
-struct DataDirectories {}
+struct DataDir {
+    virtual_address: u32,
+    size: u32,
+}
+
+struct DataDirectories {
+    export: DataDir,
+    import: DataDir,
+    resource: DataDir,
+    exception: DataDir,
+    certificate: DataDir,
+    base_relocation: DataDir,
+    debug: DataDir,
+    architecture: DataDir,
+    global_ptr: DataDir,
+    tls: DataDir,
+    load_config: DataDir,
+    bound_import: DataDir,
+    iat: DataDir,
+    delay_import_descriptor: DataDir,
+    clr_runtime_header: DataDir,
+    reserved: DataDir,
+}
 
 struct PortableExecutable {
     ms_dos_stub: Option<Vec<u8>>,
