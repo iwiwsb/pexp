2227c00ba69f454ef6cf9acdaaea79980aed6f50
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2022-06-19 00:32:07 +0300

    reimplement optional header

--- src/portable_executable/optional_headers/mod.rs
+++ src/portable_executable/optional_headers/mod.rs
@@ -1,111 +1,154 @@
 mod data_directories;
 
 use data_directories::DataDirectories;
+use ImageBase::*;
+use PEImageType::*;
 
 #[allow(non_snake_case)]
 pub struct OptionalHeader {
-    magic: u16,
-    major_linker_version: u8,
-    minor_linker_version: u8,
-    size_of_code: u32,
-    size_of_initialized_data: u32,
-    size_of_uninitialized_data: u32,
-    address_of_entry_point: u32,
-    base_of_code: u32,
-    base_of_data: Option<u32>,
-    image_base: u64,
-    section_alignment: u32,
-    file_alignment: u32,
-    major_operating_system_version: u16,
-    minor_operating_system_version: u16,
-    major_image_version: u16,
-    minor_image_version: u16,
-    major_subsystem_version: u16,
-    minor_subsystem_version: u16,
-    win32_version_value: u32,
-    size_of_image: u32,
-    size_of_headers: u32,
-    check_sum: u32,
-    subsystem: Subsystem,
-    dll_characteristics: u16,
-    size_of_stack_reserve: u64,
-    size_of_stack_commit: u64,
-    size_of_heap_reserve: u64,
-    size_of_heap_commit: u64,
-    loader_flags: u32,
-    number_of_RVA_and_sizes: u32,
-    data_directories: DataDirectories,
+    raw: Vec<u8>,
+    image_type: PEImageType,
+}
+
+enum ImageBase {
+    ImageBase32(u32),
+    ImageBase64(u64),
+}
+
+impl OptionalHeader {
+    fn magic(&self) -> u16 {
+        u16::from_le_bytes([self.raw[0], self.raw[1]])
+    }
+
+    fn image_type(&self) -> &PEImageType {
+        &self.image_type
+    }
+
+    fn major_linker_version(&self) -> u8 {
+        self.raw[2]
+    }
+
+    fn minor_linker_version(&self) -> u8 {
+        self.raw[2]
+    }
+
+    fn size_of_code(&self) -> u32 {
+        u32::from_le_bytes([self.raw[4], self.raw[5], self.raw[6], self.raw[7]])
+    }
+
+    fn size_of_initialized_data(&self) -> u32 {
+        u32::from_le_bytes([self.raw[8], self.raw[9], self.raw[10], self.raw[11]])
+    }
+
+    fn size_of_uninitialized_data(&self) -> u32 {
+        u32::from_le_bytes([self.raw[12], self.raw[13], self.raw[14], self.raw[15]])
+    }
+
+    fn address_of_entry_point(&self) -> u32 {
+        u32::from_le_bytes([self.raw[16], self.raw[17], self.raw[18], self.raw[19]])
+    }
+
+    fn base_of_code(&self) -> u32 {
+        u32::from_le_bytes([self.raw[20], self.raw[21], self.raw[22], self.raw[23]])
+    }
+
+    fn base_of_data(&self) -> Option<u32> {
+        match self.image_type() {
+            PE32 => Some(u32::from_le_bytes([
+                self.raw[24],
+                self.raw[25],
+                self.raw[26],
+                self.raw[27],
+            ])),
+            PE64 => None,
+        }
+    }
+
+    fn image_base(&self) -> ImageBase {
+        match self.image_type() {
+            PE32 => ImageBase32(u32::from_le_bytes([
+                self.raw[28],
+                self.raw[29],
+                self.raw[30],
+                self.raw[31],
+            ])),
+            PE64 => ImageBase64(u64::from_le_bytes([
+                self.raw[28],
+                self.raw[29],
+                self.raw[30],
+                self.raw[31],
+                self.raw[32],
+                self.raw[33],
+                self.raw[34],
+                self.raw[35],
+            ])),
+        }
+    }
+
+    fn section_alignment(&self) -> u32 {
+        u32::from_le_bytes([self.raw[32], self.raw[33], self.raw[34], self.raw[35]])
+    }
+
+    fn file_alignment(&self) -> u32 {
+        u32::from_le_bytes([self.raw[36], self.raw[37], self.raw[38], self.raw[39]])
+    }
+    fn major_operating_system_version(&self) -> u16 {
+        u16::from_le_bytes([self.raw[40], self.raw[41]])
+    }
+    fn minor_operating_system_version(&self) -> u16 {
+        u16::from_le_bytes([self.raw[42], self.raw[43]])
+    }
+    fn major_image_version(&self) -> u16 {
+        u16::from_le_bytes([self.raw[44], self.raw[45]])
+    }
+    fn minor_image_version(&self) -> u16 {
+        u16::from_le_bytes([self.raw[46], self.raw[47]])
+    }
+    fn major_subsystem_version(&self) -> u16 {
+        u16::from_le_bytes([self.raw[48], self.raw[49]])
+    }
+    fn minor_subsystem_version(&self) -> u16 {
+        u16::from_le_bytes([self.raw[50], self.raw[51]])
+    }
+    fn win32_version_value(&self) -> u32 {
+        u32::from_le_bytes([self.raw[52], self.raw[53], self.raw[54], self.raw[55]])
+    }
+    fn size_of_image(&self) -> u32 {
+        u32::from_le_bytes([self.raw[56], self.raw[57], self.raw[58], self.raw[59]])
+    }
+    fn size_of_headers(&self) -> u32 {
+        u32::from_le_bytes([self.raw[60], self.raw[61], self.raw[62], self.raw[63]])
+    }
+    fn checksum(&self) -> u32 {
+        u32::from_le_bytes([self.raw[64], self.raw[65], self.raw[66], self.raw[67]])
+    }
+    fn subsystem(&self) -> u16 {
+        u16::from_le_bytes([self.raw[68], self.raw[69]])
+    }
+    fn dll_characteristics(&self) -> u16 {
+        u16::from_le_bytes([self.raw[70], self.raw[71]])
+    }
 }
 
 impl TryFrom<[u8; 112]> for OptionalHeader {
     type Error = &'static str;
     fn try_from(buffer: [u8; 112]) -> Result<Self, Self::Error> {
-        use PEImageType::*;
-
-        let magic = u16::from_le_bytes([buffer[0], buffer[1]]);
-        let major_linker_version = buffer[2];
-        let minor_linker_version = buffer[3];
-        let size_of_code = u32::from_le_bytes([buffer[4], buffer[5], buffer[6], buffer[7]]);
-        let size_of_initialized_data =
-            u32::from_le_bytes([buffer[8], buffer[9], buffer[10], buffer[11]]);
-        let size_of_uninitialized_data =
-            u32::from_le_bytes([buffer[12], buffer[13], buffer[14], buffer[15]]);
-        let address_of_entry_point =
-            u32::from_le_bytes([buffer[16], buffer[17], buffer[18], buffer[19]]);
-        let base_of_code = u32::from_le_bytes([buffer[20], buffer[21], buffer[22], buffer[23]]);
-        let base_of_data = match PEImageType::try_from(magic)? {
-            PE32 => Some(u32::from_le_bytes([
-                buffer[24], buffer[25], buffer[26], buffer[27],
-            ])),
-            PE64 => None,
-        };
-        let win_offset: usize = match PEImageType::try_from(magic)? {
-            PE32 => 28,
-            PE64 => 24,
-        };
-        let image_base = match PEImageType::try_from(magic)? {
-            PE32 => u64::from_le_bytes([
-                buffer[win_offset],
-                buffer[win_offset + 4],
-                buffer[win_offset + 8],
-                buffer[win_offset + 12],
-                0,
-                0,
-                0,
-                0,
-            ]),
-            PE64 => u64::from_le_bytes([
-                buffer[win_offset],
-                buffer[win_offset + 4],
-                buffer[win_offset + 8],
-                buffer[win_offset + 12],
-                buffer[win_offset + 16],
-                buffer[win_offset + 20],
-                buffer[win_offset + 24],
-                buffer[win_offset + 32],
-            ]),
-        };
-        let section_alignment =
-            u32::from_le_bytes([buffer[32], buffer[33], buffer[34], buffer[35]]);
-        let file_alignment = u32::from_le_bytes([buffer[36], buffer[37], buffer[38], buffer[39]]);
-        let major_operating_system_version = u16::from_le_bytes([buffer[40], buffer[41]]);
-        let minor_operating_system_version = u16::from_le_bytes([buffer[42], buffer[43]]);
-        let major_image_version = u16::from_le_bytes([buffer[44], buffer[45]]);
-        let minor_image_version = u16::from_le_bytes([buffer[46], buffer[47]]);
-        let major_subsystem_version = u16::from_le_bytes([buffer[48], buffer[49]]);
-        let minor_subsystem_version = u16::from_le_bytes([buffer[50], buffer[51]]);
-        let win32_version_value =
-            u32::from_le_bytes([buffer[52], buffer[53], buffer[54], buffer[55]]);
-        let size_of_image = u32::from_le_bytes([buffer[56], buffer[57], buffer[58], buffer[59]]);
-        let size_of_headers = u32::from_le_bytes([buffer[60], buffer[61], buffer[62], buffer[63]]);
-        let checksum = u32::from_le_bytes([buffer[64], buffer[65], buffer[66], buffer[67]]);
-        let subsystem = u16::from_le_bytes([buffer[68], buffer[69]]);
-        let dll_characteristics = u16::from_le_bytes([buffer[70], buffer[71]]);
-        todo!()
+        match PEImageType::try_from([buffer[0], buffer[1]]) {
+            Ok(PE32) => Ok(OptionalHeader {
+                raw: buffer.to_vec(),
+                image_type: PE32,
+            }),
+            Ok(PE64) => Ok(OptionalHeader {
+                raw: buffer.to_vec(),
+                image_type: PE64,
+            }),
+            Err(s) => panic!("{s}"),
+        }
     }
 }
 
 #[allow(non_camel_case_types)]
+#[derive(Debug, Clone)]
 enum PEImageType {
     PE32,
     PE64,
