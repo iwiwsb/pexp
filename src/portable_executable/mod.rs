8fc0265d37e2050efdecafbbf5c42600e251343c
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2022-07-07 21:40:06 +0300

    Implement format guessing

--- src/portable_executable/mod.rs
+++ src/portable_executable/mod.rs
@@ -5,7 +5,7 @@ mod section_table;
 use coff_header::COFFFileHeader;
 use optional_headers::OptionalHeader;
 use section_table::SectionTable;
-use std::io::{self, Read, Seek, SeekFrom};
+use std::io::{self, ErrorKind, Read, Seek, SeekFrom};
 
 use coff_header::MachineType;
 
@@ -32,11 +32,17 @@ struct Reader<R: Read> {
 
 impl<R: Read> Reader<R> {
     fn new(inner: R) -> Self {
-        Self { inner, pe_type: None }
+        Self {
+            inner,
+            pe_type: None,
+        }
     }
 
     fn with_type(inner: R, pe_type: PEType) -> Self {
-        Self { inner, pe_type: Some(pe_type) }
+        Self {
+            inner,
+            pe_type: Some(pe_type),
+        }
     }
 
     fn pe_type(&self) -> Option<PEType> {
@@ -51,32 +57,35 @@ impl<R: Read> Reader<R> {
 impl<R: Read + Seek> Reader<R> {
     fn with_guessed_type(mut self) -> io::Result<Self> {
         let mut first_2_bytes = [0u8; 2];
-        let mut reader = self.inner;
-        reader.read(&mut first_2_bytes)?;
+        self.inner.read(&mut first_2_bytes)?;
         if first_2_bytes == [b'M', b'Z'] {
-            reader.seek(SeekFrom::Start(0x3C))?;
+            self.inner.seek(SeekFrom::Start(0x3C))?;
             let mut pe_signature_offset = [0u8; 4];
-            reader.read(&mut pe_signature_offset)?;
-            reader.seek(SeekFrom::Start(u32::from_le_bytes(pe_signature_offset) as u64))?;
+            self.inner.read(&mut pe_signature_offset)?;
+            self.inner.seek(SeekFrom::Start(
+                u32::from_le_bytes(pe_signature_offset) as u64
+            ))?;
             let mut pe_signature = [0u8; 4];
-            reader.read(&mut pe_signature)?;
+            self.inner.read(&mut pe_signature)?;
             if pe_signature == [b'P', b'E', 0, 0] {
-                reader.seek(SeekFrom::Current(20))?;
+                self.inner.seek(SeekFrom::Current(20))?;
                 let mut pe_magic = [0u8; 2];
-                reader.read(&mut pe_magic)?;
+                self.inner.read(&mut pe_magic)?;
                 match pe_magic {
-                    [0x0B, 0x01] => Ok(Self { inner: reader, pe_type: Some(PEType::Image32)}),
-                    [0x0B, 0x02] => Ok(Self { inner: reader, pe_type: Some(PEType::Image64)}),
-                    [0x07, 0x01] => Ok(Self { inner: reader, pe_type: Some(PEType::ImageRom)}),
-                    _ => todo!()
+                    [0x0B, 0x01] => self.pe_type = Some(PEType::Image32).or(self.pe_type),
+                    [0x0B, 0x02] => self.pe_type = Some(PEType::Image64).or(self.pe_type),
+                    [0x07, 0x01] => self.pe_type = Some(PEType::ImageRom).or(self.pe_type),
+                    _ => return Err(io::Error::from(ErrorKind::InvalidData)),
                 }
+                return Ok(self);
             } else {
-                todo!()
+                return Err(io::Error::from(ErrorKind::InvalidData));
             }
         } else if MachineType::try_from(first_2_bytes).is_ok() {
-             todo!()
+            self.pe_type = Some(PEType::Object).or(self.pe_type);
+            return Ok(self);
         } else {
-            todo!()
+            return Err(io::Error::from(ErrorKind::InvalidData));
         }
     }
 }
