5e5affda3c4bedd94e793c14f4a7a05e77bffa27
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2023-09-27 16:34:04 +0300

    Move signature to separate const

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use header::machine_types::Machine;
 use parser::PortExeType;
 use std::io::{self, ErrorKind, Read, Seek};
 
+const MZ_SIGNATURE: [u8; 2] = [b'M', b'Z'];
+
 fn main() -> io::Result<()> {
     Ok(())
 }
@@ -15,7 +17,7 @@ fn detect_pe_type<R: Read + Seek>(reader: &mut R) -> io::Result<PortExeType> {
     let mut mz = [0u8; 2];
     reader.rewind()?;
     reader.read_exact(&mut mz)?;
-    if mz == [b'M', b'Z'] {
+    if mz == MZ_SIGNATURE {
         Ok(PortExeType::Image)
     } else if Machine::try_from(mz).is_ok() {
         Ok(PortExeType::Object)
