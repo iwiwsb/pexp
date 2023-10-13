2e1ce81c0074630163db48f1ec13f04c03a0a198
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2023-10-13 17:14:37 +0300

    Delete unnecessary impls

--- src/struct_parse/mod.rs
+++ src/struct_parse/mod.rs
@@ -6,30 +6,6 @@ pub struct StructField<T: Debug> {
     pub data: T,
 }
 
-impl<T: Debug> StructField<T> {
-    pub fn new(offset: u64, data: T) -> Self {
-        Self { offset, data }
-    }
-}
-
-impl StructField<u16> {
-    pub fn raw_le_bytes(&self) -> Vec<u8> {
-        self.data.to_le_bytes().to_vec()
-    }
-}
-
-impl StructField<u32> {
-    pub fn raw_le_bytes(&self) -> Vec<u8> {
-        self.data.to_le_bytes().to_vec()
-    }
-}
-
-impl StructField<u64> {
-    pub fn raw_le_bytes(&self) -> Vec<u8> {
-        self.data.to_le_bytes().to_vec()
-    }
-}
-
 pub trait ReadU16LE {
     fn read_u16_le(&mut self, offset: u64) -> u16;
 }
