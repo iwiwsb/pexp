1d76e2343449f1339d063facb8c598ad14b00496
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2023-11-06 19:51:12 +0300

    Fix DllCharacteristics

--- src/header/win_subsystem.rs
+++ src/header/win_subsystem.rs
@@ -1,37 +1,50 @@
 //! The following values defined for the [`subsystem`](crate::header::optional_header::OptionalHeader#structfield.subsystem) field of the [`OptionalHeader`](crate::header::optional_header::OptionalHeader)
 //! determine which Windows subsystem (if any) is required to run the image.
 
-pub struct WinSubSystem {
-    flags: [bool; 16],
+pub enum WinSubSystem {
+    Unknown,
+    Native,
+    WindowsCUI,
+    WindowsGUI,
+    OS2CUI,
+    PosixCUI,
+    NativeWindows,
+    WindowsCEGUI,
+    EFIApplication,
+    EFIBootServiceDriver,
+    EFIRuntimeDriver,
+    EFIROM,
+    XBOX,
+    WindowsBootApplication,
 }
 
 impl WinSubSystem {
     /// An unknown subsystem
-    pub const IMAGE_SUBSYSTEM_UNKNOWN: u16 = 0x0000;
+    pub const IMAGE_SUBSYSTEM_UNKNOWN: u16 = 0;
     /// Device drivers and native Windows processes
-    pub const IMAGE_SUBSYSTEM_NATIVE: u16 = 0x0001;
+    pub const IMAGE_SUBSYSTEM_NATIVE: u16 = 1;
     /// The Windows graphical user interface (GUI) subsystem
-    pub const IMAGE_SUBSYSTEM_WINDOWS_GUI: u16 = 0x0002;
+    pub const IMAGE_SUBSYSTEM_WINDOWS_GUI: u16 = 2;
     /// The Windows character subsystem
-    pub const IMAGE_SUBSYSTEM_WINDOWS_CUI: u16 = 0x0003;
+    pub const IMAGE_SUBSYSTEM_WINDOWS_CUI: u16 = 3;
     /// The OS/2 character subsystem
-    pub const IMAGE_SUBSYSTEM_OS2_CUI: u16 = 0x0005;
+    pub const IMAGE_SUBSYSTEM_OS2_CUI: u16 = 5;
     /// The Posix character subsystem
-    pub const IMAGE_SUBSYSTEM_POSIX_CUI: u16 = 0x0007;
+    pub const IMAGE_SUBSYSTEM_POSIX_CUI: u16 = 7;
     /// Native Win9x driver
-    pub const IMAGE_SUBSYSTEM_NATIVE_WINDOWS: u16 = 0x0008;
+    pub const IMAGE_SUBSYSTEM_NATIVE_WINDOWS: u16 = 8;
     /// Windows CE
-    pub const IMAGE_SUBSYSTEM_WINDOWS_CE_GUI: u16 = 0x0009;
+    pub const IMAGE_SUBSYSTEM_WINDOWS_CE_GUI: u16 = 9;
     /// An Extensible Firmware Interface (EFI) application
-    pub const IMAGE_SUBSYSTEM_EFI_APPLICATION: u16 = 0x000A;
+    pub const IMAGE_SUBSYSTEM_EFI_APPLICATION: u16 = 10;
     /// An EFI driver with boot services
-    pub const IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER: u16 = 0x000B;
+    pub const IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER: u16 = 11;
     /// An EFI driver with run-time services
-    pub const IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER: u16 = 0x000C;
+    pub const IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER: u16 = 12;
     /// An EFI ROM image
-    pub const IMAGE_SUBSYSTEM_EFI_ROM: u16 = 0x000D;
+    pub const IMAGE_SUBSYSTEM_EFI_ROM: u16 = 13;
     /// XBOX
-    pub const IMAGE_SUBSYSTEM_XBOX: u16 = 0x000E;
+    pub const IMAGE_SUBSYSTEM_XBOX: u16 = 14;
     /// Windows boot application.
-    pub const IMAGE_SUBSYSTEM_WINDOWS_BOOT_APPLICATION: u16 = 0x0010;
+    pub const IMAGE_SUBSYSTEM_WINDOWS_BOOT_APPLICATION: u16 = 16;
 }
