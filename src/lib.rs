5a1de08bddfafc997ab0f2df7f83bb19b1e4beae
iwiwsb <30194859+iwiwsb@users.noreply.github.com>
2023-12-17 21:45:23 +0300

    Starting over (yes, again :clown:)

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
-use std::io::Read;
-
-pub trait ReadArray {
-    fn read_array<R: Read, const N: usize>(reader: &mut R) -> [u8; N] {
-        let mut buf = [0u8; N];
-        reader
-            .read_exact(&mut buf)
-            .expect("Data stream should be readable");
-        buf
-    }
+struct FieldInfo<T, const N: usize> {
+    offset: u64,
+    name: String,
+    raw_bytes: [u8; N],
+    value: T,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,1 @@
-pub mod file_header;
-pub mod optional_header;
-pub mod section;
-
-use std::{
-    fs::File,
-    io::{Read, Seek},
-};
-
-fn main() {
-    let mut pe_file = File::open("target\\release\\pexp.exe").expect("File should be openable");
-    let mut buf = [0u8; 4];
-    let _ = pe_file.seek(std::io::SeekFrom::Start(0x3C));
-    let _ = pe_file.read_exact(&mut buf);
-    let _ = pe_file.seek(std::io::SeekFrom::Start(u32::from_le_bytes(buf) as u64 + 4));
-}
+fn main() {}
